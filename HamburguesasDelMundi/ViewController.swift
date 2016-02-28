bc795bef92f004f86c2a52eae4b03347568c27a0
Katherine Gonzalez <katherinegonzalez@Katherines-Mac-mini.local>
2016-02-27 21:02:48 -0600

    hamburguesas

--- HamburguesasDelMundi/ViewController.swift
+++ HamburguesasDelMundi/ViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    var hamburguesas = ColeccionDeHamburguesa()
+    
+    var paises = ColeccionDePaises()
+    
+    @IBOutlet weak var labelPais: UILabel!
+    
+    @IBOutlet weak var labelHamburguesa: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func cambiar(sender: AnyObject) {
+       labelHamburguesa.text = hamburguesas.obtenHamburguesa()
+        labelPais.text = paises.obtenPais()
+        
+    }
 
 }
 
---
+++ HamburguesasDelMundi/datos.swift
@@ -0,0 +1,29 @@
+//
+//  datos.swift
+//  HamburguesasDelMundi
+//
+//  Created by Katherine Gonzalez on 27/2/16.
+//  Copyright (c) 2016 Katherine Gonzalez. All rights reserved.
+//
+
+import Foundation
+
+class ColeccionDePaises{
+    var paises :[String] = ["Costa Rica", "Afganistán","Albania","Alemania","Italia","Bélgica","Belice", "Eslovenia","España", "Estados Unidos","Estonia","Etiopía","Filipinas","Finlandia", "Panamá","Paraguay","Perú","Polonia","Portugal","Reino Unido"]
+    
+    func obtenPais( )->String{
+        var index = Int(arc4random_uniform(UInt32(paises.count)))
+        return paises [index]
+    }
+
+
+}
+class ColeccionDeHamburguesa {
+    var hamburguesas :[String] = ["Big Mac","Woper Junior","Rustica","Vegetariana","Vegana","Con Queso", "Home Burger", "The New York Burger","Vía Vélez", "De pescado", "Cuarto de libra", "Mega", " Doble torta", "Americana", " Frita", "Del Fogon", "jalapeña", "De pollo", "Coronel", "Agnus" ]
+
+    func obtenHamburguesa( )->String{
+        var index = Int(arc4random_uniform(UInt32(hamburguesas.count)))
+        return hamburguesas [index]
+    }
+
+}
