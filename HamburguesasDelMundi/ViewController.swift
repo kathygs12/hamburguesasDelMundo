4517fd8d87434122be653f9b48427fc11f4b0e03
Katherine Gonzalez <katherinegonzalez@Katherines-Mac-mini.local>
2016-02-27 19:37:15 -0600

    Initial Commit

---
+++ HamburguesasDelMundi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HamburguesasDelMundi
+//
+//  Created by Katherine Gonzalez on 27/2/16.
+//  Copyright (c) 2016 Katherine Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
